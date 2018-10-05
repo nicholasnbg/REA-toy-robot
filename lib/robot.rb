08618c7276e547db00682ee71358fd834d7733ca
Nick Gray <nicholasnbg@gmail.com>
2018-10-05 23:18:37 +1000

    Added Robot class with placed? method + specs

---
+++ lib/robot.rb
@@ -0,0 +1,11 @@
+class Robot
+  attr_accessor :current_position
+
+  def initialize
+    @current_position = nil
+  end
+
+  def placed?
+    current_position != nil
+  end
+end
---
+++ spec/robot_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe Robot do
+  let(:robot) { described_class.new }
+
+  describe '#placed?' do
+    subject { robot.placed? }
+    context 'robot is not placed' do
+      it 'returns false' do
+        expect(subject).to eq false
+      end
+    end
+
+    context 'robot is placed' do
+      before { robot.current_position = true }
+
+      it 'returns true' do
+        expect(subject).to eq true
+      end
+    end
+  end
+end
