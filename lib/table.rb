ae73bd4a562c9322504a005735f346cab5a6aa26
Nick Gray <nicholasnbg@gmail.com>
2018-10-06 17:36:32 +1000

    Minor formatting

--- lib/table.rb
+++ lib/table.rb
@@ -5,9 +5,6 @@ class Table
   end
 
   def valid_position?(position)
-    position.x <= @width  &&
-    position.x >= 0       &&
-    position.y <= @length &&
-    position.y >= 0
+    position.x <= @width  && position.x >= 0 && position.y <= @length && position.y >= 0
   end
 end
