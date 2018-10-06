adca87cf24e6de265b82bc9448eaed2fbecf9ae9
Nick Gray <nicholasnbg@gmail.com>
2018-10-06 17:53:26 +1000

    Created Command class and initialized spec file

---
+++ lib/command.rb
@@ -0,0 +1,12 @@
+class Command
+  def initialize(robot, execute, table = nil, position = nil, )
+    @robot = robot,
+    @execute = execute,
+    @table = table,
+    @position = position
+  end
+
+  def execute
+
+  end
+end
