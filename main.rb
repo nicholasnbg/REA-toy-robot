358302770ce41cc9465bcd86155d0d2b5d651466
Nick Gray <nicholasnbg@gmail.com>
2018-10-09 22:35:10 +1100

    Extracted test file runner functionality and switched logic to case statement

--- main.rb
+++ main.rb
@@ -1,12 +1,11 @@
-require_relative 'lib/robot'
-require_relative 'lib/table'
-require_relative 'lib/commander'
+Dir[File.join(__dir__, 'lib', '**', '*.rb')].each { |file| require file }
 require_relative './dialogue'
-require_relative 'lib/error_reporter'
 
 robot = Robot.new
 table = Table.new(5,5)
-commander = Commander.new(robot, table)
+parser = Parser.new
+commander = Commander.new(robot, table, parser)
+test_runner = TestFileRunner.new(commander, './tests/')
 
 Dialogue.intro_dialogue
 
@@ -15,39 +14,20 @@ loop do
   puts 'Command >> '
   puts ''
 
-  input = gets
+  input = gets.downcase
 
-  # ----------------Exit case-------------------#
-  if input.downcase.match(/^exit$/)
+  case input
+  when /^exit$/
     puts 'Thanks for playing!'
-  end
-
-  break if input.downcase.match(/^exit$/)
-
-  # --------------Help Dialogue-----------------#
-  if input.downcase.match(/^help$/)
+    break
+  when /^help$/
     Dialogue.help_dialogue
-
-  # -------------Test file runner---------------#
-  elsif input.downcase.match(/\Atest/)
-    filename = input.split(' ')[1]
-    file = "./tests/#{filename}"
-
-    if File.exist?(file)
-      File.readlines(file).each do |line|
-        if !line.match(/\A#/)
-          command = commander.return_command(line)
-          command.execute
-        else
-          puts line
-        end
-      end
-    else
-      ErrorReporter.new("no_test_file")
-    end
-  # --------------Run user input-----------------#
+  when /\Atest/
+    test_file = input.split[1]
+    test_runner.run(test_file)
   else
     command = commander.return_command(input)
     command.execute
   end
+
 end
