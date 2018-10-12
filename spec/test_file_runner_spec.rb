22f3631e3868fd1dfba464355a1897116ed2192c
Nick Gray <nicholasnbg@gmail.com>
2018-10-12 11:31:42 +1100

    More testing for test file runner

--- spec/test_file_runner_spec.rb
+++ spec/test_file_runner_spec.rb
@@ -6,7 +6,6 @@ describe TestFileRunner do
   let(:test_file_runner) { described_class.new(commander ,test_dir) }
 
   describe '#run' do
-    let(:test_file_name) { 'test_a.txt' }
     let(:file_path) { "#{test_dir}#{test_file_name}" }
 
     subject { test_file_runner.run(test_file_name) }
@@ -16,6 +15,7 @@ describe TestFileRunner do
     end
 
     context 'test file exists' do
+      let(:test_file_name) { 'test_a.txt' }
       let(:test_file_exists) { true }
       let(:line1) { 'some_line' }
       let(:line2) { 'some other line' }
@@ -26,11 +26,37 @@ describe TestFileRunner do
         allow(File).to receive(:readlines).with(file_path) { text_file_lines }
         allow(commander).to receive(:return_command) { command }
         allow(command).to receive(:execute)
+        subject
       end
 
       it 'executes the command for each line' do
-        subject
         expect(command).to have_received(:execute).twice
+      end
+    end
+
+    context 'test file does not exist' do
+      let(:test_file_name) { 'test_a.txt' }
+      let(:test_file_exists) { false }
+
+      before do
+        allow(ErrorReporter).to receive(:error)
+        subject
+      end
+      it 'runs ErrorReporter' do
+        expect(ErrorReporter).to have_received(:error)
+      end
+    end
+
+    context 'no test file argument given' do
+      let(:test_file_exists) { false }
+      let(:test_file_name) { false }
+
+      before do
+        allow(ErrorReporter).to receive(:error)
+        subject
+      end
+      it 'runs ErrorReporter' do
+        expect(ErrorReporter).to have_received(:error)
       end
     end
   end
