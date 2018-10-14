79e31881d9355cca1c98e93e26bb9fcf809c26c9
Nick Gray <nicholasnbg@gmail.com>
2018-10-14 13:02:03 +1100

    Created Tests for Command class

--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Command do
+  let(:robot) { instance_double("Robot") }
+  let(:table) { instance_double("Table") }
+  let(:position) { instance_double"Position"}
 
+  let(:command) { described_class.new(robot, table, position) }
+
+  describe '#execute' do
+    subject {command.execute}
+
+    before do
+      allow(ErrorReporter).to receive(:error)
+      subject
+    end
+
+    it 'raises MethodNotImplementedError' do
+      expect(ErrorReporter).to have_received(:error).with(RoboError::MethodNotImplemented.new)
+    end
+  end
 end
